e11df237adb07740b1549d48f9349fcbb863aeb9
rajahwu <vmradford@gmail.com>
2022-12-27 18:46:11 -0500

    complete functional test pset part one

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -53,11 +53,15 @@ suite('Functional Tests', function () {
 });
 
 const Browser = require('zombie');
+const browser = new Browser();
+browser.site = 'http://localhost:3000/';
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
-
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function () {
@@ -68,15 +72,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
   });
 });
