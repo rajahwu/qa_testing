54c32786df3cac37bef16f85adc2515902cdf0a8
rajahwu <vmradford@gmail.com>
2022-12-28 13:17:43 -0500

    fix error

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -85,7 +85,7 @@ suite('Functional Tests with Zombie.js', function() {
       browser.fill('surname', 'Colombo').then(() => {
         browser.pressButton('submit', () => {
           browser.assert.success();
-          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#name', 'Cristofo');
           browser.assert.text('span#surname', 'Colombo');
           browser.assert.elements('span#dates', 1);
           done();
